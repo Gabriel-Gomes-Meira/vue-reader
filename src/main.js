283936c15744671b9ead952265d89d5d13ff1eaf
Gabriel-Gomes-Meira <gabrielgomesm.contato@gmail.com>
2023-02-26 22:21:27 -0300

    wip

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+import 'flowbite';
 
 const app = createApp(App).use(store)
-
+import '../public/output.css'
 app.use(router).mount('#app')
